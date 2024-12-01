1f02e9c8602b21502f64329b58cd8a7fc746e2b7
Ivan Falcao <ivanpfalcao@outlook.com>
2024-12-01 19:47:54 -0300

    Graceful shutdown

--- k8s/01-deploy-stock-predictions.sh
+++ k8s/01-deploy-stock-predictions.sh
@@ -2,4 +2,6 @@ BASEDIR="$( cd "$( dirname "${0}" )" && pwd )"
 
 NAMESPACE="datalake-ns"
 
+kubectl -n ${NAMESPACE} delete deployment stock-predictions
+
 kubectl -n ${NAMESPACE} apply -f "${BASEDIR}/stock_predictions.yaml"
